e0adbb89fc6ae1fe72c9e7ebd5123c388ab2b74d
BermotMonoki <madiata.md@gmail.com>
2026-02-16 15:46:46 +0100

    Version finale après test

--- age_calculator.py
+++ age_calculator.py
@@ -1,14 +1,14 @@
 import datetime
 
 def main():
-    birth_year = 0  
+    birth_year = int(input(print("Enter your birth year: ")))
 
-    current_year = 0 
+    current_year = datetime.datetime.now().year
 
-    age = 0
+    age = current_year - birth_year
 
-    print("--------------------")
-    print(f"Current Year: {current_year}")
+    #print("--------------------")
+    #print(f"Current Year: {current_year}")
     print(f"You are approximately {age} years old.")
 
 if __name__ == "__main__":
